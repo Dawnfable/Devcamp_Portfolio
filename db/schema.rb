529465cfe7770b2e17d6e5f49a20e74820b09c23
Dawnfable <DuskDawnfable@gmail.com>
2018-02-01 14:04:46 -0700

    added info for petergate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180126212607) do
+ActiveRecord::Schema.define(version: 20180201205945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,6 @@ ActiveRecord::Schema.define(version: 20180126212607) do
     t.string "name"
     t.bigint "portfolio_id"
     t.datetime "created_at", null: false
-    
     t.datetime "updated_at", null: false
     t.index ["portfolio_id"], name: "index_technologies_on_portfolio_id"
   end
@@ -86,6 +85,7 @@ ActiveRecord::Schema.define(version: 20180126212607) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "roles"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
